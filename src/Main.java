8bc157f065e8a53dc4872e2186051792597805a9
Alina Mihaela <ciopecalina@gmail.com>
2024-04-07 16:56:40 +0300

    Modified quotes

--- src/Main.java
+++ src/Main.java
@@ -233,26 +233,26 @@ class Candies implements ISurprise {
 class FortuneCookie implements ISurprise {
 
     private static final String[] quotes = {
-            "Cand vei fi multumit sa fii pur si simplu tu insuti si sa nu te compari cu ceilalti, toti te vor respecta. (Lao Tse)",
-            "Odata ce ai ales speranta, totul este posibil. (Christopher Reeve)",
-            "Am invatat tacerea de la cei vorbareti, toleranta de la cei intoleranti si bunatatea de la cei rai. (Khalil Gibran)",
-            "Cel ce n-are nici un merit invidiaza intotdeauna meritele altora. (Francis Bacon)",
-            "Cea mai inalta masura a valorii o dai de fapt in timpul confruntarilor la care te supune viata. (Anonim)",
-            "Am descoperit ca daca iubesti viata, si viata te va iubi pe tine. (Arthur Rubinstein)",
-            "Fiecare experienta de viata, fiecare lectie pe care o invat este cheia spre viitorul meu. (Clark Gable)",
-            "Prima conditie pentru a fi fericit este sa n-ai timp sa te gandesti la nefericire. (George Bernard Shaw)",
-            "Da tot ce ai mai bun in tine, intrucat totul din viata ta iti apartine numai tie. (Ralph Waldo Emerson)",
-            "Nebun este cine traieste in propria-i lume. (Paulo Coelho)",
-            "Pentru orice rau exista doua leacuri: timpul si tacerea. (Alexandre Dumas)",
-            "In viata lucrurile se fac usor, doar ideea gresita ca ar fi greu ne opreste sa fim extraordinari. (Marian Rujoiu)",
-            "S-ar putea ca actiunea sa nu-ti ofere fericire, dar nu exista fericire in lipsa actiunii. (William James)",
-            "Nu vad niciodata ce s-a facut. Vad doar ce mai e de facut. (Buddha)",
-            "Nu-ti pierde timpul batand intr-un perete sperand ca il vei transforma intr-o usa. (Coco Chanel)",
-            "Ataseaza-te de cei care te pot face mai bun si primeste-i pe cei care, la randul tau, ii poti face mai buni. (Seneca)",
-            "Mai bine sa te consumi decat sa ruginesti. (Denis Diderot)",
-            "Intelept este acela care traieste in fiecare zi ca si cum in fiecare zi si in fiecare ceas ar putea sa moara. (Francisco Gomez de Quevedo)",
-            "Toti avem in noi o nestiuta rezerva de energie care izbucneste cand viata ne pune la incercare. (Isabel Allende)",
-            "Stelele nu pot straluci fara intuneric. (Anonim)"
+            "When you are content to simply be yourself and not compare yourself to others, everyone will respect you. (Lao Tse)",
+            "Once you choose hope, anything is possible. (Christopher Reeve)",
+            "I learned silence from the talkative, tolerance from the intolerant and kindness from the wicked. (Khalil Gibran)",
+            "He who has no merit always envies the merits of others. (Francis Bacon)",
+            "The highest measure of value is actually given during the confrontations to which life subjects you. (Anonymous)",
+            "I discovered that if you love life, life will love you. (Arthur Rubinstein)",
+            "Every life experience, every lesson I learn is the key to my future. (Clark Gable)",
+            "The first condition to be happy is not to have time to think about unhappiness. (George Bernard Shaw)",
+            "Give the best you have in you, because everything in your life belongs to you alone. (Ralph Waldo Emerson)",
+            "He who lives in his own world is crazy. (Paulo Coelho)",
+            "For every evil there are two cures: time and silence. (Alexandre Dumas)",
+            "In life things are easy, only the wrong idea that it would be difficult stops us from being extraordinary. (Marian Rujoiu)",
+            "Action may not bring you happiness, but there is no happiness without action. (William James)",
+            "I never see what has been done. I only see what remains to be done. (Buddha)",
+            "Don't waste your time knocking on a wall hoping you'll turn it into a door. (Coco Chanel)",
+            "Attach yourself to those who can make you better and welcome those who, in turn, you can make better. (Seneca)",
+            "Better to consume than to rust. (Denis Diderot)",
+            "Wise is the one who lives every day as if every day and every hour he could die. (Francisco Gomez de Quevedo)",
+            "We all have in us an unknown reserve of energy that bursts forth when life puts us to the test. (Isabel Allende)",
+            "Stars cannot shine without darkness. (Anonymous)"
     };
 
     private String message;
